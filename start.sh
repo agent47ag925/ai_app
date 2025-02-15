6450c180dfabf1926afbd8e1773c6bf187974fa4
jeongeun park <47ag925park@gmail.com>
2025-02-16 08:29:31 +0900

    clouds3

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 streamlit run main.py --server.port=$PORT --server.address=0.0.0.0
-uvicorn fastapp:app --reload --host=0.0.0.0 --port=${PORT}
+uvicorn fastapp:app --reload --host=0.0.0.0 --port=8000
