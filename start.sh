31ca38e72a5a1855d7eaedb7eeb7735dba8daa22
jeongeun park <47ag925park@gmail.com>
2025-02-16 10:30:00 +0900

    port and url3

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 API_PORT=8001 python fastapp.py &
-streamlit run main.py --server.port=$PORT --server.address=0.0.0.0 
+streamlit run main.py --server.port=$PORT --server.address=0.0.0.0 &
+
+wait
